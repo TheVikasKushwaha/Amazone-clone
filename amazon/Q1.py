df9f9a129fcac7161d4019d077ad8fdc87906850
Vikas Kushwaha <vikaskushwaha18082004@gmail.com>
2025-09-19 20:24:02 +0530

    Add files via upload

---
+++ amazon/Q1.py
@@ -0,0 +1,39 @@
+# num1=76
+# num2=56
+# print("the sum of the given number is",num1+num2)
+#user se input lene ke liye
+# num1=float(input("enter the first numer"))
+# num2=float(input("enter the second number"))
+# print("the sum of the number is",num1+num2)
+# print("hello world")
+# a="hello world"
+# print(a)
+
+# num = float(input("enter the number to take the sqare root"))
+# print("the sqaure of given number is",num**(1/2))
+
+# num1=float(input("enter the first numer"))
+# num2=float(input("enter the second number"))
+# print("the sum of the number is",num1**num2)
+
+# height=float(input("enter the height of thr triangle"))
+# base=float(input("enter the base of thr triangle"))
+# print("the area of the triangle is:",(1/2)*base*height)
+# x=float(input("enter the x value"))
+# y=float(input("enter the y value"))
+# temp=x
+# x=y
+# y=temp
+# print("the swappingb value of x is :",x)
+# print("the swappingb value of y is :",y)
+# distance=float(input("Enter the distance in kilometre"))
+# miles=distance*0.621371
+# print("the distance in miles is:",miles)
+a=float(input("Enter the number"))
+if(a<=-1):
+    print("the number is negative")
+elif(a>=1):
+    print("the number is positive")
+elif(a==0):
+    print("the number is zero")    
+
